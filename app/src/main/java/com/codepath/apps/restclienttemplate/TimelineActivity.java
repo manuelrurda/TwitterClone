704887bf99b9715aab9d1daee979e8248cf611a5
manuelrurda <manuelrurda@gmail.com>
2022-06-07 15:36:46 -0700

    Log Out Button

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.codepath.apps.restclienttemplate.Adapters.TweetsAdapter;
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -69,6 +71,17 @@ public class TimelineActivity extends AppCompatActivity {
                 Log.d(TAG, "onFailure!", throwable);
             }
         });
+    }
 
+    // Click handler for the logout button
+    public void onClickLogOut(View view){
+        //finish(); // TODO: ask?
+        // forget who's logged in
+        TwitterApp.getRestClient(this).clearAccessToken();
+
+        Intent i = new Intent(this, LoginActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
     }
 }
